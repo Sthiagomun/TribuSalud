7ae5748bd1cfc9fc31a86f6297f0c672f235c5d4
Santiago Muñoz <srmc152007@hotmail.com>
2025-04-14 20:00:30 -0500

    Cambios del login y creacion de algunos otros archivos

---
+++ login/login_index.js
@@ -0,0 +1,7 @@
+document.getElementById('login-button').onclick = function() {
+    window.location.href = 'dashboard.html'; // Cambia 'dashboard.html' por la URL de destino para "Acceder"
+};
+
+document.getElementById('register-button').onclick = function() {
+    window.location.href = 'register.html'; // Cambia 'register.html' por la URL de destino para "Registrarse"
+};
