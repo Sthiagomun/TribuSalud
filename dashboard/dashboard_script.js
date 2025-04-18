a2988f41cd434dd08be9919902140b0750e72e5a
Melany Malambo <melanymalambo@gmail.com>
2025-04-18 18:29:24 -0500

    Se agregaron ajustes en la pantalla de inicio y se conecto boton de cerrar sesion

--- dashboard/dashboard_script.js
+++ dashboard/dashboard_script.js
@@ -1,3 +1,7 @@
 document.querySelector('.profile-button').onclick = function () {
     window.location.href = '../profile/profile_index.html';
 };
+
+document.querySelector('.Cerrar-Sesion').onclick = function () {
+    window.location.href = '../login/login.html';
+};
