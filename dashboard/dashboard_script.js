4eafafa1ab81dcd7c56c71c887fc5fb228c8bd52
Santiago Muñoz <srmc152007@hotmail.com>
2025-04-21 21:49:06 -0500

    Se acomo btones del dashboard

--- dashboard/dashboard_script.js
+++ dashboard/dashboard_script.js
@@ -12,24 +12,15 @@ document.querySelector('.btnSolicitarCita').onclick = function () {
 
 document.querySelector('.btnHistorialClinico').onclick = function () {
     window.location.href = '../medical_history/medical_history_index.html';
-<<<<<<< HEAD
 }; /*boton del historial clinico conectado */
 
 document.querySelector('.Citas-y-Turnos').onclick = function () {
-    window.location.href = '../request_an_appointment/request_an_appointment_index.html';
+    window.location.href = '../request_an_appointment/request_index.html';
 }; /*boton de citas y turnos conectado */
 
 document.querySelector('.btnCancelarCita').onclick = function () {
     window.location.href = '../appointment_cancellation/appointment_cancellation_index.html';
 }; /*boton de cancelar cita conectado */
-=======
-}; /*boton del solicitar cita conectado */
 document.querySelector('.Actualizar-Datos').onclick = function () {
     window.location.href = '../actualizacion de datos/actualizaciondatos_index.html';
-}
-/*boton del solicitar cita conectado */
-document.querySelector('.Citas-y-Turnos').onclick = function () {
-    window.location.href = '../request_an_appointment/request_index.html';
-}
-/*boton del solicitar cita conectado */
->>>>>>> 992dde046c870ed4d391cec8623b5f29615aa80a
+}; /*boton de receta conectado */
