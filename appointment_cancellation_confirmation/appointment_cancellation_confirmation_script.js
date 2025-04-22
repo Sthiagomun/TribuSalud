88160ed19e750772c213cbea5da3c508fa29aae6
Santiago Muñoz <srmc152007@hotmail.com>
2025-04-21 21:40:43 -0500

    Se ha acomodad la cancelacion de citas

--- appointment_cancellation_confirmation/appointment_cancellation_confirmation_script.js
+++ appointment_cancellation_confirmation/appointment_cancellation_confirmation_script.js
@@ -1,6 +1,22 @@
-document.querySelector('.btnAceptar').onclick = function () {
-    window.location.href = '../dashboard/dashboard_index.html';
-}; 
-document.querySelector('.Btn-Inicio').onclick = function () {
-    window.location.href = '../dashboard/dashboard_index.html';
-}; 
+document.addEventListener('DOMContentLoaded', function () {
+    const btnAceptar = document.querySelector('.btnAceptar');
+    if (btnAceptar) {
+        btnAceptar.onclick = function () {
+            window.location.href = '../dashboard/dashboard_index.html';
+        };
+    }
+
+    const btnInicio = document.querySelector('.btnInicio');
+    if (btnInicio) {
+        btnInicio.onclick = function () {
+            window.location.href = '../dashboard/dashboard_index.html';
+        };
+    }
+
+    const btnCerrarSesion = document.querySelector('.btnCerrarSesion');
+    if (btnCerrarSesion) {
+        btnCerrarSesion.onclick = function () {
+            window.location.href = '../login/login.html';
+        };
+    }
+});
